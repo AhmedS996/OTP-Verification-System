b6aa931f588ca3b7d4c9cc9656f7b80c31a8a6af
Ahmed Alsaiqal <91352253+AhmedS996@users.noreply.github.com>
2023-06-03 02:12:13 +0300

    adding comments

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php 
-session_start();
-require 'php/Connection.php';
+session_start(); // Start a new session or resume an existing session
+require 'php/Connection.php'; // Include the database connection code
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,26 +18,22 @@ require 'php/Connection.php';
             <input tabindex = "1"type="email" name="Email" placeholder="email" maxlength="40" required>
         </label>
         <?php 
-            if(isset($_POST['submit'])){
-                $_SESSION['Email']= $Email = $_POST['Email'];
-                $dquery = "DELETE FROM `otp` WHERE email = '$Email'";
+            if(isset($_POST['submit'])){ // If the form has been submitted
+                $_SESSION['Email']= $Email = $_POST['Email']; // Store the email in a session variable
+                $dquery = "DELETE FROM `otp` WHERE email = '$Email'"; // Delete any existing OTP for the email from the database
 
-                mysqli_query($conn,$dquery);
-                $passcode= rand(100000, 999999);// create random number of OTP with 6 digit
+                mysqli_query($conn,$dquery); // Execute the delete query
+                $passcode= rand(100000, 999999);// Generate a random 6-digit OTP
 
-                //? Insert email and passcode to database - for verification 
-                $Insert_otp = "INSERT INTO `otp` (`passcode`,`email`) VALUES ('$passcode','$Email')";
-                                        
+                // Insert the email and OTP into the database for verification
+                $Insert_otp = "INSERT INTO `otp` (`passcode`,`email`) VALUES ('$passcode','$Email')";                                      
                 $query = mysqli_query($conn,$Insert_otp);
 
-                $_SESSION['v']=1;
-                header("Location: php/Sent-OTP.php");
+                $_SESSION['v']=1; // Set a session variable to indicate that the OTP has been sent
+                header("Location: php/Sent-OTP.php"); // Redirect the user to the "Sent-OTP.php" page
             }
         ?>
         <button tabindex ="2" type="submit" class="submit" name="submit">Sign In</button>
     </form>
-                        
-   
-        
 </body>
 </html>
