6f47f982f1a7785a7ef7217d4654f11841040950
Ahmed Alsaiqal <91352253+AhmedS996@users.noreply.github.com>
2023-06-02 23:32:02 +0300

    added timer for OTP verfication

---
+++ JS/Timer.js
@@ -0,0 +1,12 @@
+//timer to input otp code
+
+var timeleft = 60;
+var downloadTimer = setInterval(function () {
+  if (timeleft <= 0) {
+    window.location.replace("../Timer_Finished.php");
+  } else {
+    document.getElementById("countdown").innerHTML =
+      timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
