7e1a3b944b012f0887c7c9e0235d21f8c04d60a0
Ahmed Alsaiqal <91352253+AhmedS996@users.noreply.github.com>
2023-06-03 02:11:11 +0300

    make it shows as m and s

--- JS/Timer.js
+++ JS/Timer.js
@@ -1,12 +1,23 @@
-//timer to input otp code
+// Set the initial time remaining to 300 seconds (5 minutes)
+var timeleft = 300;
 
-var timeleft = 60;
+// Update the countdown timer every second using an interval timer
 var downloadTimer = setInterval(function () {
+  // If the time has run out, redirect to the "Timer_Finished.php" page
   if (timeleft <= 0) {
-    window.location.replace("../php/Timer_Finished.php");
+    window.location.replace("Timer_Finished.php");
   } else {
-    document.getElementById("countdown").innerHTML =
-      timeleft + " seconds remaining";
+    // Compute the number of minutes and seconds remaining
+    var minutes = Math.floor(timeleft / 60);
+    var seconds = timeleft % 60;
+
+    // Construct a string that displays the time remaining in minutes and seconds
+    var countdownText = minutes + "m " + seconds + "s remaining";
+
+    // Update the HTML element with the ID "countdown" to display the countdown text
+    document.getElementById("countdown").innerHTML = countdownText;
   }
+
+  // Decrement the time remaining by one second
   timeleft -= 1;
 }, 1000);
