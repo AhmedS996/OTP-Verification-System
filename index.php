743be50d258c1e7615029bdf1beddca8722a0dbe
Ahmed Alsaiqal <91352253+AhmedS996@users.noreply.github.com>
2023-06-02 23:33:14 +0300

    added improtatn pages

---
+++ index.php
@@ -0,0 +1,26 @@
+<!-- home page -->
+
+<?php 
+//this for cannot go through index.php until you need to login first
+session_start();
+if(empty($_SESSION['id'])){ 
+  header("Location: login.php");
+ }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page</title>
+</head>
+<body>
+    <h1>Hi you are in home page</h1>
+    <p>Your Email is:<?php echo "{$_SESSION['Email']}";?> </p>
+    <p>Your First name:<?php echo "{$_SESSION['Fname']}";?></p>
+    <p>Your Last name:<?php echo "{$_SESSION['Lname']}";?></p>
+    
+    <p><a class="logout-a" href="logout.php">logout</a></p>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,39 @@
+<?php 
+session_start();
+require 'php/Connection.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login/signup page</title>   
+</head>
+<body>
+    <form action="login.php" method="post">
+        <h1>Welcome to login page</h1>
+        <label>
+            <label for="Email">Enter Email</label>
+            <input tabindex = "1"type="email" name="Email" placeholder="email" maxlength="40" required>
+        </label>
+        <?php 
+            if(isset($_POST['submit'])){
+                $_SESSION['Email']= $Email = $_POST['Email'];
+                    
+                $passcode= rand(100000, 999999);// create random number of OTP with 6 digit
+                 
+                //? Insert email and passcode to database - for verification 
+                $Insert_otp = "INSERT INTO `otp` (`passcode`,`email`) VALUES ('$passcode','$Email')";
+                                        
+                $query = mysqli_query($conn,$Insert_otp);
+                header("Location: php/Sent-OTP.php");
+            }
+        ?>
+        <button tabindex ="2" type="submit" class="submit" name="submit">Sign In</button>
+    </form>
+                        
+   
+        
+</body>
+</html>
